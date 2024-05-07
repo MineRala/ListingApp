95f4b12162b0febd97ff9c40c46830c0049f9841
Mine Rala <mine.rala@rapsodo.com>
2024-05-07 14:03:43 +0300

    Initial Commit

---
+++ ListingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListingApp
+//
+//  Created by Mine Rala on 7.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
